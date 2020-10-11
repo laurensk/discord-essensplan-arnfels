7879c4d2c69a87ba239cf0ca4d27bbee38288a68
Laurens K <39766854+laurensk@users.noreply.github.com>
2020-10-11 18:36:47 +0200

    utc time changes

--- src/Scheduling.ts
+++ src/Scheduling.ts
@@ -4,7 +4,7 @@ import { PdfLinkDownloader } from "./PdfLinkDownloader";
 
 export class Scheduling {
   public static enable(callback: Function) {
-    Schedule.scheduleJob("0 5 * * 1-5", function () {
+    Schedule.scheduleJob("0 7 * * 1-5", function () {
       PdfLinkDownloader.getNewest((link) => {
         Axios.post(
           "Guten Morgen! Der Essensplan für heute: https://discordapp.com/api/webhooks/764516615806124083/ggkimYNiNX5KueRTdXNWPTZdkIAxT9sG7xdhOMzim-26ZO4rwZiltXN954OzZGH73H0p",
