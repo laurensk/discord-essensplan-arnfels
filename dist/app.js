7c757b9b3c85ad64e0e83350ef49c91ae143ac4b
Laurens Kropf <39766854+laurensk@users.noreply.github.com>
2020-10-11 18:34:38 +0200

    Delete app.js

--- dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Scheduling_1 = require("./Scheduling");
-Scheduling_1.Scheduling.enable(() => {
-    console.log("Successfully started Essensplan-Arnfels...\n");
-});
-//# sourceMappingURL=App.js.map
